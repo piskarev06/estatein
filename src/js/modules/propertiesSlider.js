9868326d279a0e9bbf4a8d96ec4dab591da961ae
Danila <msabirova34@gmail.com>
2024-02-29 20:02:35 +0300

    working with adaptive

--- src/js/modules/propertiesSlider.js
+++ src/js/modules/propertiesSlider.js
@@ -2,32 +2,43 @@ import {
   decrementCount,
   incrementCount,
   addArrowListener,
-  setCountOfSlides,
-} from "./counterHelpers";
+  setCountOfSlides
+} from './counterHelpers';
 
 export const propertiesSlider = () => {
-  const propertiesSlider = new Swiper(".properties__slider", {
-    direction: "horizontal",
+  const propertiesSlider = new Swiper('.properties__slider', {
+    direction: 'horizontal',
     slidesPerView: 3,
     spaceBetween: 30,
     watchSlidesProgress: true,
     loop: false,
     simulateTouch: false,
     navigation: {
-      nextEl: ".properties-button-next",
-      prevEl: ".properties-button-prev",
+      nextEl: '.properties-button-next',
+      prevEl: '.properties-button-prev'
     },
+    breakpoints: {
+      391: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+
+      1605: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
   });
 
-  setCountOfSlides("properties__card", "properties__count");
+  setCountOfSlides('properties__card', 'properties__count');
   addArrowListener(
-    "properties__arrow--prev",
-    "properties__currentCount",
+    'properties__arrow--prev',
+    'properties__currentCount',
     decrementCount
   );
   addArrowListener(
-    "properties__arrow--next",
-    "properties__currentCount",
+    'properties__arrow--next',
+    'properties__currentCount',
     incrementCount
   );
 };
--- src/js/modules/reviewsSlider.js
+++ src/js/modules/reviewsSlider.js
@@ -2,32 +2,43 @@ import {
   setCountOfSlides,
   addArrowListener,
   decrementCount,
-  incrementCount,
-} from "./counterHelpers";
+  incrementCount
+} from './counterHelpers';
 
 export const reviewsSlider = () => {
-  const reviewsSlider = new Swiper(".reviews__slider", {
-    direction: "horizontal",
+  const reviewsSlider = new Swiper('.reviews__slider', {
+    direction: 'horizontal',
     slidesPerView: 3,
     spaceBetween: 30,
     watchSlidesProgress: true,
     loop: false,
     simulateTouch: false,
     navigation: {
-      nextEl: ".reviews-button-next",
-      prevEl: ".reviews-button-prev",
+      nextEl: '.reviews-button-next',
+      prevEl: '.reviews-button-prev'
     },
+    breakpoints: {
+      391: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+
+      1605: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
   });
 
-  setCountOfSlides("reviews__card", "reviews__count");
+  setCountOfSlides('reviews__card', 'reviews__count');
   addArrowListener(
-    "reviews__arrow--prev",
-    "reviews__currentCount",
+    'reviews__arrow--prev',
+    'reviews__currentCount',
     decrementCount
   );
   addArrowListener(
-    "reviews__arrow--next",
-    "reviews__currentCount",
+    'reviews__arrow--next',
+    'reviews__currentCount',
     incrementCount
   );
 };
